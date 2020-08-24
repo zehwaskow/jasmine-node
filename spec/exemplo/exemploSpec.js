f25c6f6bf94a7e8027b2c4f1a0236dfdb03a29f0
ze <zehwaskow@gmail.com>
2020-08-24 17:48:25 -0300

    Arquivos do projeto local

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,8 @@
+describe('Suite de teste de exemplo com NodeJS', () => {
+    it('deve demonstrar o uso do jasmine com o NodeJS', function () {
+        expect(true).toEqual(true);
+    });
+    it('deve demonstrar o uso do karma em modo de escuta', () => {
+        expect(1).toEqual(1);
+    });
+});
